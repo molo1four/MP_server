36577684f5b3790d272f588052bd553e2e828b77
molo1four <adnee@naver.com>
2020-08-27 15:08:15 +0900

    auth

--- routes/movies.js
+++ routes/movies.js
@@ -6,6 +6,6 @@ const { getMovies, AddLikes } = require("../controllers/movies");
 const router = express.Router();
 
 // 각 경로 별로 데이터 가져올 수 있도록, router 셋팅
-router.route("/").get(getMovies).post(AddLikes);
+router.route("/").get(auth, getMovies).post(auth, AddLikes);
 
 module.exports = router;
