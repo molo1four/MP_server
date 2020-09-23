594d093d3d317490c8ceedd42dde0d65d0dcf437
molo1four <adnee@naver.com>
2020-09-23 16:51:51 +0900

    11

--- routes/movies.js
+++ routes/movies.js
@@ -13,7 +13,7 @@ const router = express.Router();
 
 // 각 경로 별로 데이터 가져올 수 있도록, router 셋팅
 router.route("/").get(auth, getMovies).post(auth, AddLikes);
-router.route("/doLikes").get(auth, getMovies_nl);
+router.route("/likedMovie").get(auth, getMovies_nl);
 router.route("/getRecom").get(auth, getRecom);
 router.route("/getRecom_AR").get(auth, getRecom_AR);
 module.exports = router;
